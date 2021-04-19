3de8f3bcafd5254fba08ddd296103c5346b381b3
yuu-eguci <yuu.eguci@gmail.com>
2021-04-19 20:50:13 +0900

    ✨ spread_sheet_result_sender.py

--- spread_sheet_result_sender.py
+++ spread_sheet_result_sender.py
@@ -4,7 +4,7 @@
 
 python spread_sheet_result_sender.py
 
-したとき、一番下にある 5.6.1.3.8 のデータが「レース 65 の着順」として
+したとき、一番下にあるデータが「レース 65 の着順」として
 Spread Sheet にきちんと格納されるように、関数 send を作ってください!
 """
 
@@ -19,4 +19,15 @@ def send(race_number, numbers_str):
 
 
 if __name__ == '__main__':
-    send('65', '5.6.1.3.8')
+    send('65', {
+        'tansho_payout': 360,
+        'umaren_payout': 670,
+        'umatan_payout': 1280,
+        'fuku3_payout': 2660,
+        'tan3_payout': 10400,
+        'ranking1': 4,
+        'ranking2': 18,
+        'ranking3': 2,
+    })
+
+
